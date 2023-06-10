434110e20309be69602aa1b24ecb2b9a46cd9c7f
Sarah S <stms15@mun.ca>
2023-06-10 11:12:35 -0230

    added a fucntion to process the received data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # weather-forecast-dashboard
+
+https://www.w3schools.com/js/js_loop_forof.asp
+
+https://www.w3schools.com/jsref/jsref_max.asp
--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,110 @@ function getGeoCoordinates(cityName, appId) {
     });
 }
 
+function determineDailyMain(array) {
+  var numRain = 0;
+  var numClouds = 0;
+  var numSnow = 0;
+  var numExtreme = 0;
+  var numClear = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "Rain") {
+      numRain++;
+    } else if (array[i] === "Clouds") {
+      numClouds++;
+    } else if (array[i] === "Snow") {
+      numSnow++;
+    } else if (array[i] === "Extreme") {
+      numExtreme++;
+    } else if (array[i] === "Clear") {
+      numClear++;
+    } else {
+      console.log("Error: unknown main weather type");
+    }
+  }
+
+  var mostCommon = Math.max(numRain, numClouds, numSnow, numExtreme, numClear);
+  if (mostCommon === numRain) {
+    return "Rain";
+  } else if (mostCommon === numClouds) {
+    return "Clouds";
+  } else if (mostCommon === numSnow) {
+    return "Snow";
+  } else if (mostCommon === numExtreme) {
+    return "Extreme";
+  } else if (mostCommon === numClear) {
+    return "Clear";
+  }
+  return;
+}
+
+function processWeatherData(weatherData) {
+  var forecast = [
+    {
+      temp: 0,
+      temp_min: 0,
+      temp_max: 0,
+      humidity: 0,
+      main: [],
+      pop: 0,
+    },
+    {
+      temp: 0,
+      temp_min: 0,
+      temp_max: 0,
+      humidity: 0,
+      main: [],
+      pop: 0,
+    },
+    {
+      temp: 0,
+      temp_min: 0,
+      temp_max: 0,
+      humidity: 0,
+      main: [],
+      pop: 0,
+    },
+    {
+      temp: 0,
+      temp_min: 0,
+      temp_max: 0,
+      humidity: 0,
+      main: [],
+      pop: 0,
+    },
+    {
+      temp: 0,
+      temp_min: 0,
+      temp_max: 0,
+      humidity: 0,
+      main: [],
+      pop: 0,
+    },
+  ];
+  var variablesToStore = ["temp", "temp_min", "temp_max", "humidity"];
+
+  for (let day = 0; day < 5; day++) {
+    for (let i = 0; i < 8; i++) {
+      var index = i + day * 8;
+      for (let variable of variablesToStore) {
+        forecast[day][variable] += weatherData.list[index].main[variable];
+      }
+      forecast[day].pop += weatherData.list[index].pop;
+      forecast[day].main.push(weatherData.list[index].weather[0].main);
+    }
+    // Average and convert to deg C
+    forecast[day].temp = forecast[day].temp / 8 - 273.15;
+    forecast[day].temp_min = forecast[day].temp_min / 8 - 273.15;
+    forecast[day].temp_max = forecast[day].temp_max / 8 - 273.15;
+    forecast[day].humidity = forecast[day].humidity / 8; // percentage
+    forecast[day].main = determineDailyMain(forecast[day].main);
+    forecast[day].pop = (forecast[day].pop / 8) * 100; // percentage
+  }
+
+  console.log(forecast);
+}
+
 // ----------------------- //
 
 var ApiKey = "090e889d7a08a33b213911545eb4136f";
@@ -40,6 +144,7 @@ searchBttnEl.addEventListener("click", function (event) {
 
   getGeoCoordinates(cityToSearch, ApiKey);
   coordinates = JSON.parse(localStorage.getItem("coordinates"));
+  console.log(coordinates);
 
   var reqUrl =
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
@@ -56,9 +161,12 @@ searchBttnEl.addEventListener("click", function (event) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      processWeatherData(data);
+      console.log(data.list);
     })
     .catch(function (error) {
       console.log(error);
     });
 });
+
+// NOTE: this is working for cases when the first input is 03:00:00 of day 1. This does not account for requesting the weather in the middle of the day and getting the first input for later than 03:00:00.
